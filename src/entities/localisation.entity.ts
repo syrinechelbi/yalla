46f06ac9f0af826156f9f41053d756c0745df516
khaoula <khaoulamessaoud452@gmail.com>
2024-01-19 12:15:57 +0100

    location+enum

---
+++ src/entities/localisation.entity.ts
@@ -0,0 +1,19 @@
+
+
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Localisation {
+  @PrimaryGeneratedColumn()
+  idLocalisation: number;
+
+  @Column()
+  ville: string;
+
+  @Column()
+  rue: string;
+
+  @Column()
+  codePostal: number;
+
+}
